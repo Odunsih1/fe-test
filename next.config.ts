2be08e74375a3e5646fef93428d6e9009be31727
HENRY ODUNSI <n.rytech15@gmail.com>
2026-01-09 02:01:45 +0100

    feat: Init, update dependencies and add new icons
    - Updated package.json to include "svgr" as a dependency.
    - Removed several unused SVG files from the public directory.
    - Added new SVG icons for account, customer, dashboard, food, help, manage, notification, payment, search, and setting.
    - Created new UI components for Button and Input.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,55 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, 
+      },
+
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                ],
+              },
+              titleProp: true,
+              ref: true,
+            },
+          },
+        ],
+      }
+    );
+
+
+    fileLoaderRule.test = /\.(png|jpe?g|gif|webp|avif|ico|bmp)$/i;
+
+    return config;
+  },
 };
 
 export default nextConfig;
